a56e281c5479096c8bf7fe0307997242798ba117
P.Sai Rishi <sairishipalavalasa1108@gmail.com>
2025-07-07 14:06:13 +0530

    domain

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,20 +1,19 @@
 
+
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  console.log("generate token triggered")
+  console.log("generate token triggered");
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  console.log(token)
+  console.log(token);
 
-  var x=res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 15,
+    sameSite: "None",     // 👈 Required for cross-site cookies
+    secure: true          // 👈 Required for HTTPS (like Render & Vercel)
   });
-  console.log(x)
-
   return token;
 };
