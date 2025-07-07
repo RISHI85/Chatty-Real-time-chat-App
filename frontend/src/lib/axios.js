91a3ce801020474c3b9928b60739fea187ebaa21
P.Sai Rishi <sairishipalavalasa1108@gmail.com>
2025-07-07 13:35:53 +0530

    axios instance changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api",
+    baseURL : "https://chatty-real-time-chat-app-028i.onrender.com",
     withCredentials:true,
 })
